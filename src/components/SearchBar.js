7948abefeb6b9c02ad36f689757351b0db9d6c35
Ryan <kim1885@purdue.edu>
2019-04-11 20:25:03 -0400

    add vertical spacing between images

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -16,7 +16,7 @@ class Searchbar extends React.Component {
       <div className="ui segment">
         <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
-            <label> Image Search</label>
+            <label> Image Search </label>
             <input 
               type="text" 
               value={this.state.term} 
